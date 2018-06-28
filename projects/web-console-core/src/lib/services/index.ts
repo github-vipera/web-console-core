cfb5bc685467f8f84ccac8cccb7acf86ef8c86a1
Enrico <enrico.mazzucchelli@vipera.com>
2018-06-28 20:51:35 +0200

    Fix aot bug

--- projects/web-console-core/src/lib/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './web-admin-plugin-manager';
-export * from './motif-connector';
-export * from './auth-service';
-export * from './navigation-service'
-
-
--- projects/web-console-core/src/public_api.ts
+++ projects/web-console-core/src/public_api.ts
@@ -2,8 +2,11 @@
  * Public API Surface of web-console-core
  */
 
-export * from "./lib/commons"
-export * from "./lib/services"
-export * from "./lib/components"
+export * from "./lib/commons/PluginRegistry"
+export * from "./lib/services/auth-service/auth.service"
+export * from "./lib/services/motif-connector/motif-connector.service"
+export * from "./lib/services/navigation-service/navigation.service"
+export * from "./lib/services/web-admin-plugin-manager/web-admin-plugin-manager.service"
+export * from "./lib/components/web-admin-console/web-admin-console.component"
 export * from "./lib/web-admin-core.module"
 
