2649afa79767666e454b5573d2a79af589731505
Marco Bonati <marco.bonati@vipera.com>
2018-06-29 09:59:05 +0200

    Added Goto Dashboard feature

--- projects/web-console-core/src/lib/components/page-not-found/page-not-found.component.ts
+++ projects/web-console-core/src/lib/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationService } from '../../services/navigation-service/navigation.service';
 
 @Component({
   selector: 'web-admin-page-not-found',
@@ -6,4 +7,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './page-not-found.component.html'
 })
 export class PageNotFoundComponent {
+
+  public constructor(private navService:NavigationService) {
+    console.log("PageNotFoundComponent component constructor");
+  }
+
+  onReturnToDashboardClick(){
+    this.navService.goToDashboard();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ const routes:Routes = [
   {
     path:"**",
     component:PageNotFoundComponent
-  }
+    }
 ]
 
 @NgModule({
