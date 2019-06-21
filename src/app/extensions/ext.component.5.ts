f0d6bc39fc194d771430acb98ef8161705c7da41
Marco Tormento <marco.tormento@vipera.com>
2019-06-21 11:01:13 +0200

    Upgraded deps.

--- src/app/extensions/ext.component.5.ts
+++ src/app/extensions/ext.component.5.ts
@@ -8,8 +8,7 @@ import { PluginView,MotifConnectorService } from 'web-console-core';
   styleUrls: ['./ext.component.css']
 })
 @PluginView("extf", {
-  iconName: 'wa-ico-log',
-  index: 1
+  iconName: 'wa-ico-log'
 }
 )
 export class ExtComponentF implements OnInit{
